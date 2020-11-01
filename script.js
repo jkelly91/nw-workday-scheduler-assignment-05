a3bf69c2a8e621c521f8da75ee7af77ac1140946
Joseph Kelly <jkelly2191@gmail.com>
2020-11-01 01:37:22 -0500

    add bootstrap and html row, buttons, columns

--- script.js
+++ script.js
@@ -5,20 +5,4 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-  // var row = $('<div>').attr('class','.row')
-  // var timeBlock = $('<div>').attr('class','.time-block col-2')
-  // var desc = $('<div>').attr('class','.description col-8')
-  // var saveButton = $('<div>').attr('class','.saveBtn')
-
-  // $('.container').append(saveButton)
-
-})  
+})
