2fd0b2c8c96d71a786b6b9a2c4bbe279045926fc
Joseph Kelly <jkelly2191@gmail.com>
2020-11-02 15:26:11 -0600

    create click function to listen for time and text

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
   // gets current date from moment.js
   $('#currentDay').text(moment().format('dddd, MMMM Do'))
 
+  // assigns saveBtn to listen to time and text on click
+  $('.saveBtn').on('click', function(){
+
+    console.log(this)
+    var text = $(this).siblings('.description').val()
+    var time = $(this).parent().attr('id')
+
+    // Save input in local storage
+    localStorage.setItem(time, text)
+
+  })
+
 })
